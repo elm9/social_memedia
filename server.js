a3cdcad658597f4e73b3e30d8a5f621f01674507
nominb <nominboldbaatar4@gmail.com>
2018-10-09 14:16:53 -0400

    login stuff

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ var PORT = process.env.PORT || 8080;
 // Requiring our models for syncing
 var db = require("./models");
 
+//Requiring our AWS S-3
+var AWS = require(‘aws-sdk’);
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
