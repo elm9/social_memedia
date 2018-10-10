698fd1d0c7fb52d34f919c317e985ab035f71f7c
em <6em.rva@gmail.com>
2018-10-10 18:37:26 -0400

    added s3 bucket

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,31 +1,76 @@
 var db = require("../models");
-var passport = require("../config/passport");
+const AWS = require("aws-sdk");
+var bucketName = 'socialmemedia';
+var bucketRegion = 'us-east-1';
+AWS.config.update({
+  region: bucketRegion
+});
+var s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  params: {
+    Bucket: bucketName
+  }
+});
+var document = require("../views");
 
 module.exports = function (app) {
 
 
-app.post("/api/register", function (req, res) {
-  // Create an Author with the data available to us in req.body
-  console.log(req.body);
-  db.profile.create(req.body).then(function (user) {
-    res.json(user);
+  app.post("/api/login", function (req, res) {
+
+    // Create an Author with the data available to us in req.body
+    console.log(req.body);
+    db.profile.create(req.body).then(function (user) {
+      res.json(user);
+      createAlbum();
+    });
+
+    function createAlbum(albumName) {
+      albumName = albumName.trim();
+      if (!albumName) {
+        return alert('Usernames must contain at least one non-space character.');
+      }
+      if (albumName.indexOf('/') !== -1) {
+        return alert('Usernames cannot contain slashes.');
+      }
+      var albumKey = encodeURIComponent(albumName) + '/';
+      s3.headObject({
+        Key: albumKey
+      }, function (err, data) {
+        if (!err) {
+          return alert('Username already exists.');
+        }
+        if (err.code !== 'NotFound') {
+          return alert('There was an error: ' + err.message);
+        }
+        s3.putObject({
+          Key: albumKey
+        }, function (err, data) {
+          if (err) {
+            return alert('There was an error: ' + err.message);
+          }
+          alert('Successfully created account.');
+        });
+      });
+    }
   });
-});
 
 
-app.post("/api/login", passport.authenticate('local', {
-   // if the username and password match an entry in the database, redirect them to the home feed. 
-  // If not alert them that their username or password is incorrect
-    successRedirect: '/feed',
-    failureRedirect: '/login',
-    failureFlash: true
-  })
- 
 
 
-);
+  app.post("/api/login", passport.authenticate('local', {
+      // if the username and password match an entry in the database, redirect them to the home feed. 
+      // If not alert them that their username or password is incorrect
+      successRedirect: '/feed',
+      failureRedirect: '/login',
+      failureFlash: true
+    })
 
-//when user name and password matches we direct user to feed.handlebars
+
+
+  );
+
+  //when user name and password matches we direct user to feed.handlebars
 
 
 
--- server.js
+++ server.js
@@ -21,20 +21,53 @@ var db = require("./models");
 var app = express();
 
 
+// sets up AWS s3 and console.log bucket
+// =============================================================
 //Requiring our AWS S-3
-var AWS = require("aws-sdk");
+const AWS = require("aws-sdk");
+const S3_BUCKET = process.env.S3_BUCKET;
+
+var credentials = new AWS.SharedIniFileCredentials({
+  profile: 'default'
+});
+AWS.config.credentials = credentials;
+// Set the region 
+AWS.config.update({
+  region: 'us-east-1'
+});
+// Create S3 service object
+s3 = new AWS.S3({
+  apiVersion: '2006-03-01'
+});
+// Call S3 to list current buckets
+s3.listBuckets(function (err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Bucket List", data.Buckets);
+  }
+});
+// =============================================================
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 
 // Static directory
 app.use(express.static("public"));
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(session({
+  secret: "keyboard cat",
+  resave: true,
+  saveUninitialized: true
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser());
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 
 // Routes
@@ -45,8 +78,10 @@ require("./routes/html_routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync({
+  force: true
+}).then(function () {
+  app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
 });
