341155dd03976ce08a0a4eaaec2c3b251f7417de
PCFilicetti <peter.filicetti@kingsmantech.com>
2018-10-12 11:30:51 -0400

    still working on feed, fixed register

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -14,23 +14,31 @@ module.exports = function (app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads create.html
-  app.get("/", function(req, res) {
-    res.render(path.join(__dirname, "../views/index"), { title: "Social Memedia - Home" });
+  app.get("/", function (req, res) {
+    res.render(path.join(__dirname, "../views/index"), {
+      title: "Social Memedia - Home"
+    });
   });
 
-  app.get("/register", function(req, res) {
-    res.render(path.join(__dirname, "../views/register"), { title: "Social Memedia - Register" });
+  app.get("/register", function (req, res) {
+    res.render(path.join(__dirname, "../views/register"), {
+      title: "Social Memedia - Register"
+    });
   });
 
   // cms route loads feed.html
-  app.get("/feed", isMemed, function(req, res) {
-    res.render(path.join(__dirname, "../views/feed"), { title: "Social Memedia - Feed" });
+  app.get("/feed", isMemed, function (req, res) {
+    res.render(path.join(__dirname, "../views/feed"), {
+      title: "Social Memedia - Feed"
+    });
   });
 
-    // cms route loads feed.html
-    app.get("/login", function(req, res) {
-      res.render(path.join(__dirname, "../views/login"), { title: "Social Memedia - Login" });
+  // cms route loads feed.html
+  app.get("/login", function (req, res) {
+    res.render(path.join(__dirname, "../views/login"), {
+      title: "Social Memedia - Login"
     });
+  });
 
   // // blog route loads index.html
   // app.get("/index", function(req, res) {
