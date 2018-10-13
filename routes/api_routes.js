ba4cecd05342af1b54d3f25b48402a8f787104e3
em <6em.rva@gmail.com>
2018-10-13 13:45:40 -0400

    add photo

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,18 +1,20 @@
 const db = require("../models");
 const passport = require("../config/passport");
+
+// ===============================================
 const AWS = require("aws-sdk");
 const Busboy = require('busboy');
 const bucketName = 'socialmemedia';
 const bucketRegion = 'us-east-1';
-
 AWS.config.update({
   region: bucketRegion
 });
 AWS.config.apiVersions = {
   s3: '2006-03-01',
 };
-
 const s3 = new AWS.S3();
+// ==============================================
+
 
 module.exports = function (app) {
 
@@ -59,17 +61,18 @@ module.exports = function (app) {
   
   // upload image to S3
   app.post("/api/upload", function (req, res) {
-    const folder = (req.user.username + "/")
-    console.log("Folder name: " + folder);
+    const folder = (req.user.username + "/");
     const file = (req.body.imageUpload);
-    console.log("File: " + file);
     const params = {
       Bucket: bucketName,
       Key: (folder + file),
       ACL: 'public-read',
       Body: file
     };
-
+    console.log("Folder name: " + folder);
+    console.log("File: " + file);
+    
+    // uploads image in folder in bucket
     s3.putObject(params, function (err, data) {
       if (err) {
         console.log("Error: ", err);
@@ -80,60 +83,3 @@ module.exports = function (app) {
     res.redirect("/feed");
   });
 }
-    
-    // const file = (req.body.imageUpload);
-    // const busboy = new Busboy({
-    //   headers: req.headers
-    // });
-    // busboy.on('finish', function () {
-    //   console.log('Upload finished');
-    //   console.log(file);
-    //   uploadToS3(file);
-    // });
-    // req.pipe(busboy);
-  // });
-
-
-//   function uploadToS3(file) {
-//     // console.log(req.body);
-
-//     const folder = (req.user.username + "/")
-//     console.log("Folder name: " + folder);
-//     const params = {
-//       Bucket: bucketName,
-//       Key: folder,
-//       ACL: 'public-read',
-//       Body: stream
-//     };
-
-//     s3.upload(params, function (err, data) {
-//       if (err) {
-//         console.log("Error: ", err);
-//       } else {
-//         console.log("Successfully created a folder on S3");
-        
-//       }
-//     });
-//     // res.redirect("/feed");
-//   }
-// };
-
-
-// this is the stuff that works
-    // const folder = (req.user.username + "/")
-    // console.log("Folder name: " + folder);
-    // const params = {
-    //   Bucket: bucketName,
-    //   Key: folder,
-    //   ACL: 'public-read',
-    //   Body: stream
-    // };
-
-    // s3.upload(params, function (err, data) {
-    //   if (err) {
-    //     console.log("Error: ", err);
-    //   } else {
-    //     console.log("Successfully created a folder on S3");
-        
-    //   }
-    // });
