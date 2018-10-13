afb79d18049b37e60e48b09f2f3d617f1f3b70c4
em <6em.rva@gmail.com>
2018-10-13 10:07:17 -0400

    create folder works, still working on uploading file

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,60 +1,33 @@
-var db = require("../models");
-var passport = require("../config/passport");
+const db = require("../models");
+const passport = require("../config/passport");
 const AWS = require("aws-sdk");
-const S3_BUCKET = process.env.S3_BUCKET;
-var bucketName = 'socialmemedia';
-var bucketRegion = 'us-east-1';
+const Busboy = require('busboy');
+const bucketName = 'socialmemedia';
+const bucketRegion = 'us-east-1';
+
 AWS.config.update({
   region: bucketRegion
 });
-var s3 = new AWS.S3({
-  apiVersion: '2006-03-01',
-  params: {
-    Bucket: bucketName
-  }
-});
-var document = require("../views");
+const s3 = new AWS.S3();
 
 module.exports = function (app) {
 
 
   app.post("/api/register", function (req, res) {
 
-    s3.listBuckets(function (err, data) {
-      if (err) {
-        console.log("Error", err);
-      } else {
-        console.log("Bucket List", data.Buckets);
-      }
-    });
     // Create an Author with the data available to us in req.body
     console.log(req.body);
     db.profile.create(req.body).then(function (user) {
       // res.json(user);
 
-      req.login(user, function(err) {
-        if (err) { return next(err); }
+      req.login(user, function (err) {
+        if (err) {
+          return next(err);
+        }
         return res.redirect('/feed');
       });
-
     });
-
-
   });
-
-
-
-
-  // app.post("/api/login", passport.authenticate('local', {
-  //     // if the username and password match an entry in the database, redirect them to the home feed. 
-  //     // If not alert them that their username or password is incorrect
-
-  //     successRedirect: '/feed',
-  //     failureRedirect: '/login',
-  //     failureFlash: true
-  //   })
-  // );
-
 
 
   app.post("/api/login", passport.authenticate("local"), function (req, res) {
@@ -74,53 +47,47 @@ module.exports = function (app) {
     }
   });
 
-  app.get("/logout", function(req, res) {
+  app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
   });
 
 
-
-  // app.post("/api/login", passport.authenticate("local"), function(req, res) {
-  //   res.json("/feed");
-  // });
-
-  // when user name and password matches we direct user to feed.handlebars
-
-
-  // when you need to crate an account
-  // app.post("/api/profile", ){
-  //   function createAlbum(albumName) {
-  //     albumName = albumName.trim();
-  //     if (!albumName) {
-  //       return alert('Usernames must contain at least one non-space character.');
-  //     }
-  //     if (albumName.indexOf('/') !== -1) {
-  //       return alert('Usernames cannot contain slashes.');
-  //     }
-  //     var albumKey = encodeURIComponent(albumName) + '/';
-  //     s3.headObject({
-  //       Key: albumKey
-  //     }, function (err, data) {
-  //       if (!err) {
-  //         return alert('Username already exists.');
-  //       }
-  //       if (err.code !== 'NotFound') {
-  //         return alert('There was an error: ' + err.message);
-  //       }
-  //       s3.putObject({
-  //         Key: albumKey
-  //       }, function (err, data) {
-  //         if (err) {
-  //           return alert('There was an error: ' + err.message);
-  //         }
-  //         alert('Successfully created account.');
-  //       });
-  //     });
-  // }
+  // upload image to S3
+  app.post("/api/upload", function (req, res) {
+    const imageUpload = (req.body.imageUpload);
+    var busboy = new Busboy({
+      headers: req.headers
+    });
+    busboy.on('finish', function () {
+      console.log('Upload finished');
+      console.log(file);
+      uploadToS3(file);
+    });
+    // req.pipe(busboy);
+  });
 
 
+  function uploadToS3(file) {
+    // console.log(req.body);
 
-  // }
+    let folder = (req.user.username + "/")
+    console.log("Folder name: " + folder);
+    let params = {
+      Bucket: bucketName,
+      Key: folder,
+      ACL: 'public-read',
+      Body: file.data
+    };
 
+    s3.upload(params, function (err, data) {
+      if (err) {
+        console.log("Error: ", err);
+      } else {
+        console.log("Successfully created a folder on S3");
+        console.log(data);
+      }
+    });
+    // res.redirect("/feed");
+  }
 };
