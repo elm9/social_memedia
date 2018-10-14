b300631a801e36021c783ef70e206abcfae49e50
Peter Filicetti <peterfilicetti@outlook.com>
2018-10-14 15:41:41 -0400

    made the feed page mobile friendly :^)

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,33 +1,46 @@
-$(document).ready(function() {
+$(document).ready(function () {
   console.log("this is linked");
-    var loginForm = $(".login-form");
-    var usernameInput = $("#username");
-    var passwordInput = $("#password");
-  
-    loginForm.submit(function(event) {
-      console.log('yuss')
-      event.preventDefault();
-      var profileData = {
-        username: usernameInput.val().trim(),
-        password: passwordInput.val().trim()
-      };
-  
-      loginUser(profileData.username, profileData.password);
-      usernameInput.val("");
-      passwordInput.val("");
-    });
-  
-    function loginUser(username, password) {
-      console.log(username);
-      console.log(password);
-      $.post("/api/login", {
-        username: username,
-        password: password
-      }).then(function(data) {
+  var loginForm = $(".login-form");
+  var usernameInput = $("#username");
+  var passwordInput = $("#password");
 
-        window.location.replace(data);
-      }).catch(function(err) {
-        console.log(err);
-      });
+  loginForm.submit(function (event) {
+    console.log('yuss')
+    event.preventDefault();
+    var profileData = {
+      username: usernameInput.val().trim(),
+      password: passwordInput.val().trim()
+    };
+
+    loginUser(profileData.username, profileData.password);
+    usernameInput.val("");
+    passwordInput.val("");
+  });
+
+  function loginUser(username, password) {
+    console.log(username);
+    console.log(password);
+    $.post("/api/login", {
+      username: username,
+      password: password
+    }).then(function (data) {
+
+      window.location.replace(data);
+    }).catch(function (err) {
+      console.log(err);
+    });
+  }
+
+  $("#side-nav-toggle").on("click", function (event) {
+    event.preventDefault();
+    if ($("#side-nav-toggle").hasClass("toggled")) {
+      $("#side-nav-toggle").removeClass("toggled");
+      $("#side-nav").removeClass("open");
+      $("#container").removeClass("side-open");
+    } else {
+      $("#side-nav-toggle").addClass("toggled");
+      $("#side-nav").addClass("open");
+      $("#container").addClass("side-open");
     }
   });
+});
